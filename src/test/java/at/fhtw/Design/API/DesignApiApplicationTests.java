ac02b4bc5a66002e5e185ecfd309c8092d113173
osama_Fh <ic18b079@technikum-wien.at>
2023-01-26 17:57:47 +0100

    Initial commit

---
+++ src/test/java/at/fhtw/Design/API/DesignApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.Design.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
