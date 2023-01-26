25c2d64cb159b6d9f85d0ed5c848ab1c79a4c794
osama_Fh <ic18b079@technikum-wien.at>
2023-01-26 18:58:14 +0100

    #3  add AreaControllerTest class

--- src/test/java/at/fhtw/Design/API/AreaControllerTest.java
+++ src/test/java/at/fhtw/Design/API/AreaControllerTest.java
@@ -1,4 +1,29 @@
 package at.fhtw.Design.API;
+import at.fhtw.Design.API.Service.AreaService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(MockitoExtension.class)
 public class AreaControllerTest {
+    @Mock
+    private AreaService areaService;
+
+    @InjectMocks
+    private AreaController areaController;
+
+    @Test
+    public void testGetCircleArea() {
+        double value = 3;
+        double expected = 28.27;
+        when(areaService.calculateCircleArea(value)).thenReturn(expected);
+        double actual = areaController.getCircleArea(value);
+        assertEquals(expected, actual);
+    }
+
 }
+
