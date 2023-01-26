4c915aff745ac0233d60ca27f9aaea6f72cb15fb
osama_Fh <ic18b079@technikum-wien.at>
2023-01-26 18:52:46 +0100

    #3  add AreaControllerTest class

--- src/test/java/at/fhtw/Design/API/AreaServiceTest.java
+++ src/test/java/at/fhtw/Design/API/AreaServiceTest.java
@@ -1,4 +1,18 @@
 package at.fhtw.Design.API;
 
+import at.fhtw.Design.API.Service.AreaService;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AreaServiceTest {
+    private AreaService areaService = new AreaService();
+
+
+    @Test
+    public void testCalculateSquareArea() {
+        double value = 3;
+        double expected = 9;
+        double actual = areaService.calculateSquareArea(value);
+        assertEquals(expected, actual, 0.01);
+    }
 }
