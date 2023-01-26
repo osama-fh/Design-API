5f1ee6335983db52f306427bdd5c99859ff980fc
osama_Fh <ic18b079@technikum-wien.at>
2023-01-26 18:31:38 +0100

    #2  update AreaService class

--- src/main/java/at/fhtw/Design/API/Service/AreaService.java
+++ src/main/java/at/fhtw/Design/API/Service/AreaService.java
@@ -2,13 +2,15 @@ package at.fhtw.Design.API.Service;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class AreaService {
+    private static final double PI = 3.14;
     public double calculateCircleArea(double value) {
-        return Math.round(Math.PI * value * value * 100.0) / 100.0;
+        return PI * value * value;
     }
-
     public double calculateSquareArea(double value) {
-        return Math.round(value * value * 100.0) / 100.0;
+        return value * value;
     }
 }
+
