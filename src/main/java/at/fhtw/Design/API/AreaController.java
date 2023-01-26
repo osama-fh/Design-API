06ba393ca981742e3f7e8a516832d73ad02d1475
osama_Fh <ic18b079@technikum-wien.at>
2023-01-26 18:33:27 +0100

    #2  implement AreaController class

--- src/main/java/at/fhtw/Design/API/AreaController.java
+++ src/main/java/at/fhtw/Design/API/AreaController.java
@@ -1,2 +1,31 @@
 package at.fhtw.Design.API;
 
+import at.fhtw.Design.API.Service.AreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AreaController {
+    @Autowired
+    private AreaService areaService;
+
+    @GetMapping("/circle")
+    public double getCircleArea(@RequestParam(value = "value") double value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("Value must be greater than zero");
+        }
+        return areaService.calculateCircleArea(value);
+    }
+
+    @GetMapping("/square")
+    public double getSquareArea(@RequestParam(value = "value") double value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("Value must be greater than zero");
+        }
+        return areaService.calculateSquareArea(value);
+    }
+}
