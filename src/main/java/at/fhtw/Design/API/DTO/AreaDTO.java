27cf1b1cc3500db8e195730d85fd777095e03d3b
osama_Fh <ic18b079@technikum-wien.at>
2023-01-26 18:14:57 +0100

    #2 implementation  AreaDTO class

--- src/main/java/at/fhtw/Design/API/DTO/AreaDTO.java
+++ src/main/java/at/fhtw/Design/API/DTO/AreaDTO.java
@@ -1,4 +1,13 @@
 package at.fhtw.Design.API.DTO;
 
 public class AreaDTO {
+    private double value;
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
 }
